e635024777ca13f23ea0c60169dd463ae3c87b9b
Muhammad Abdulloh <muhammad.abdulloh@kuelap.io>
2024-10-09 10:54:45 +0700

    initial commit

---
+++ lib/stoic_quotes_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule StoicQuotesWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use StoicQuotesWeb, :controller` and
+  `use StoicQuotesWeb, :live_view`.
+  """
+  use StoicQuotesWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ priv/repo/seeds.exs
@@ -0,0 +1,26 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     StoicQuotes.Repo.insert!(%StoicQuotes.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+alias StoicQuotes.Quotes
+
+# Read quotes from the JSON file
+quotes_path = "priv/repo/quotes.json"
+quotes_path
+|> File.read!()
+|> Jason.decode!()
+|> Enum.each(fn attrs ->
+	# Construct a quote struct and attempt to insert it
+	quote = %{quote: attrs["quote"], author: attrs["author"], source: attrs["source"]}
+	case Quotes.create_quote(quote) do
+		{:ok, _quote} -> :ok
+		{:error, _changeset} -> :duplicate
+	end
+end)
